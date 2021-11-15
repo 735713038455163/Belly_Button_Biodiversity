734ffc693f137b2ea712bcbed1e1f082fa9b5737
Leslie D <88687569+735713038455163@users.noreply.github.com>
2021-11-14 19:31:07 -0500

    debug

--- js/Charts.js
+++ js/Charts.js
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("js/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +56,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("js/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
@@ -134,21 +134,26 @@ function buildCharts(sample) {
     
     // Gauge
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [{
-      domain: {x: [0,1], y: [0,1]},
-      value: wfreq,
-      title: {text: "Belly Button Washing Frequency <br> Scrubs per Week"},
-      type: "indicator",
-      mode: "gauge+number",
-      gauge: {
-        axis: {range: [null,10]},
-        bar: {color: "black"},
-        steps: [
-          {range: [0, 2], color: "red"},
-          {range: [2, 4], color: "orange"},
-          {range: [4, 6], color: "yellow"},
-          {range: [6, 8], color: "lightgreen"},
-          {range: [8, 10], color: "green"}]
+    var gaugeData = [
+      {
+        type: "indicator",
+        mode: "gauge+number",
+        domain: { x: [0, 1], y: [0, 1] },
+        value: floatWfreq,
+        title: { text: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week', font: { size: 18 } },
+        gauge: {
+          axis: { range: [null, 9],tickwidth: 1, tickcolor: "black" },
+          bar: { color: "black" },
+          bgcolor: "white",
+          borderwidth: 2,
+          bordercolor: "gray",
+          steps: [
+            { range: [0, 2], color: 'red' },
+            { range: [2, 4], color: 'orange'},
+            { range: [4, 6], color: 'yellow'},
+            { range: [6, 8], color: 'greenyellow'},
+            { range: [8, 10], color: 'green'}            
+          ]]
         }
       }
     ];
@@ -156,8 +161,9 @@ function buildCharts(sample) {
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = {
       width: 500,
-      height: 460,
-      margin: {t: 0, b: 0}
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      font: { color: "black", family: "Arial" }
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
